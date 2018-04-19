1fde536a9dea4f54c95bb3591f03f4cbeeef3941
Haku-Dell <g@kohaku.cc>
2018-04-19 17:00:04 +0800

    modify filepath

--- src/monitoring.py
+++ src/monitoring.py
@@ -55,7 +55,7 @@ def get_sites():
 
     :return:
     """
-    with open('../src/sites.json', 'rb') as f:
+    with open('sites.json', 'rb') as f:
         data = json.load(f)
 
     return data['sites']
