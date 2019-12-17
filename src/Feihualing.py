0fb7783339fecd721ff801b5a0e68fdce9e9c0be
Haku-Dell <g@kohaku.cc>
2019-12-17 18:05:13 +0800

    射覆&双飞

--- src/Feihualing.py
+++ src/Feihualing.py
@@ -23,7 +23,7 @@ def get_hot_works():
 
 def keywords_filter(works, keyword):
     """
-    筛选出works中包含有keyword的作品
+    筛选出works中包含有keyword的作品，keyword包含多个字符时只要work中有其中任意字符即为符合条件；
     :param works:
     :param keyword:
     :return:
@@ -32,6 +32,13 @@ def keywords_filter(works, keyword):
 
 
 def get_paragraphs(work):
+    """
+    根据换行符将work切分成段落列表；
+
+    注：对诗来说有用，一般情况下诗一段两联，但是对词、文等其他类型则没有意义，因为一般情况下词一段为一阙，未必是两句；
+    :param work:
+    :return:
+    """
     return re.split('\r\n|\n', work)
 
 
@@ -39,6 +46,7 @@ def get_paragraphs_all(works):
     result = []
     for work in works:
         result += get_paragraphs(work)
+    return result
 
 
 def get_single_sentence(work):
@@ -56,36 +64,48 @@ def get_single_sentence_all(works):
         result += get_single_sentence(work)
     return result
 
-# keywords = '风光'
-# print(keywords_filter(get_hot_works(), keywords))
 
-# l = get_hot_works()
-# print(set(l[0]) & set('风光四十'))
-# print(l[0])
+def shefu(sentence, kw, limit=0):
+    """
 
-# print('a' if set("asdfasdfasfd") & set("ddd") else 'b')
-
-# get_paragraphs_all(keywords_filter(get_hot_works(), '风'))
-
-# s = get_single_sentence(get_hot_works()[0])
-# print(s)
-
-# print(len(get_hot_works()))
-# print(keywords_filter(get_hot_works(), '风'))
-
-# ws = get_hot_works()
-# print(get_single_sentence(ws[0]))
-# print('----------------')
-# print(get_single_sentence(ws[1]))
-# print('----------------')
-# print(get_single_sentence(ws[0]) + get_single_sentence(ws[1]))
+    :param sentence:
+    :param kw:
+    :param limit:
+    :return:
+    """
+    r = []
+    for s in get_single_sentence_all(keywords_filter(get_hot_works(), kw)):
+        if set(s) & set(sentence) and set(s) & set(kw):
+            one = re.sub(r'[\r\n]', '', s)
+            if limit == 0:
+                r.append(one)
+            else:
+                if len(one) == limit:
+                    r.append(one)
+    print(r)
 
 
-print(len(keywords_filter(get_hot_works(), '词')))
-r=[]
-for s in get_single_sentence_all(keywords_filter(get_hot_works(), '词')):
-    # print(s)
-    if set(s) & set('三山半落青天外') and set(s) & set('词'):
-        print(s)
-        r.append(s)
-print(r)
+# shefu('甫昔少年日', '府')
+
+
+def shuangfei(s1, s2):
+    ps = get_hot_works()
+    ps = keywords_filter(ps, s1+s2)
+    print(len(ps))
+    for work in ps:
+        # 以句号分区，不同区的两句排除出双飞规则判断范围
+        sections = re.split('。', work)
+        for section in sections:
+            if not set(section) & set(s1) or not set(section) & set(s2):
+                continue
+            s = re.split('[，|！|？|]', section)
+            if len(s) < 2:
+                continue
+            for i in range(len(s)-1):
+                if set(s[i]) & set(s1) and set(s[i+1]) & set(s2):
+                    print(re.sub(r'[\r\n]', '', s[i]), re.sub(r'[\r\n]', '', s[i+1]))
+            # if set(s[0]) & set(s1) and set(s[1]) & set(s2):
+            #     print(s[0], s[1])
+
+
+shuangfei('无那尘缘容易绝', '燕子依然')
